91e1ba74cb3a29787c11eb197677185909a0a399
AKSHAY MAHAJAN <akshaymahajan@AKSHAYs-MacBook-Pro.local>
2022-05-01 16:46:53 -0400

    click listeners added for navigation

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const APP = {
+  init: () => {
+    document.getElementById("ham").addEventListener("click", APP.hamClicked);
+    document
+      .getElementById("backbone")
+      .addEventListener("click", APP.dotClicked);
+  },
+  hamClicked: (event) => {
+    const nav = event.currentTarget;
+    nav.classList.toggle("clicked");
+    if (nav.classList.contains("clicked")) {
+      nav.firstElementChild.textContent = "close";
+    } else {
+      nav.firstElementChild.textContent = "menu";
+    }
+  },
+  dotClicked: (event) => {
+    const dot = event.target.closest(".dots");
+    if (dot) {
+      event.currentTarget.querySelector(".filled").classList.remove("filled");
+      dot.classList.add("filled");
+    }
+  },
+};
+
+document.addEventListener("DOMContentLoaded", APP.init);
